e01e3f630dadbe3eed8ee37fd9f75bcdbb8379e9
Dmitry Berdnikov <berdnikovdmitry@mail.ru>
2021-11-04 17:47:46 +0300

    add login route

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
+import Login from '../Login/Login.jsx';
 import Page404 from '../Page404/Page404.jsx';
 
 export default () => (
   <div className="App">
     <Routes>
+      <Route path="/login" element={<Login />} />
       <Route path="*" element={<Page404 />} />
     </Routes>
   </div>
