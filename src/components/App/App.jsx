2eb15ea7f6a9bf42e0dcb072932f3b021be811f2
Dmitry Berdnikov <berdnikovdmitry@mail.ru>
2021-11-05 18:47:16 +0300

    add home route

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
+import Chat from '../Chat/Chat.jsx';
 import Login from '../Login/Login.jsx';
 import Page404 from '../Page404/Page404.jsx';
 
 export default () => (
   <div className="App">
     <Routes>
+      <Route path="/" element={<Chat />} />
       <Route path="/login" element={<Login />} />
       <Route path="*" element={<Page404 />} />
     </Routes>
