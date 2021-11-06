42b2290233ad4c3d2d98c6b7652a2154b84f831e
Dmitry Berdnikov <berdnikovdmitry@mail.ru>
2021-11-06 18:54:05 +0300

    add auth and update styles for Login

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -5,13 +5,14 @@ import Header from '../Header/Header.jsx';
 import Chat from '../Chat/Chat.jsx';
 import Login from '../Login/Login.jsx';
 import Page404 from '../Page404/Page404.jsx';
+import RequireAuth from '../RequireAuth/RequireAuth.jsx';
 
 export default () => (
   <AuthProvider>
     <Header />
     <div className="flex-grow-1">
       <Routes>
-        <Route path="/" element={<Chat />} />
+        <Route path="/" element={<RequireAuth><Chat /></RequireAuth>} />
         <Route path="/login" element={<Login />} />
         <Route path="*" element={<Page404 />} />
       </Routes>
--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,13 +1,18 @@
-import React from 'react';
-import { Formik } from 'formik';
+import React, { useState, useRef, useEffect } from 'react';
 import {
   FloatingLabel,
   Form,
   Row,
   Col,
   Button,
+  Container,
 } from 'react-bootstrap';
+import { Formik } from 'formik';
 import { useTranslation } from 'react-i18next';
+import axios from 'axios';
+import { useLocation, useNavigate } from 'react-router-dom';
+import routes from '../../routes';
+import { useAuth } from '../../contexts/authContext.jsx';
 
 const isSubmitDisabled = ({ values, isSubmitting }) => {
   if (isSubmitting) {
@@ -20,59 +25,89 @@ const isSubmitDisabled = ({ values, isSubmitting }) => {
 
 const Login = () => {
   const { t } = useTranslation();
+  const [authFailed, setAuthFailed] = useState(false);
+  const auth = useAuth();
+  const inputUsernameRef = useRef(null);
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  useEffect(() => {
+    inputUsernameRef.current.focus();
+  }, []);
+
   const handleSubmit = async (values) => {
-    console.log(values);
+    try {
+      const response = await axios.post(routes.loginPath(), values);
+      setAuthFailed(false);
+      localStorage.setItem('user', JSON.stringify(response.data));
+      auth.logIn();
+      const from = location.state?.from?.pathname || '/';
+      navigate(from, { replace: true });
+    } catch (err) {
+      if (err.isAxiosError && err.response.status === 401) {
+        setAuthFailed(true);
+        inputUsernameRef.current.focus();
+        return;
+      }
+
+      throw err;
+    }
   };
 
   return (
-    <Row className="py-5">
-      <Col xs="6" className="mx-auto">
-        <h2 className="mb-5">{t('log in')}</h2>
-        <Formik
-          initialValues={{ username: '', password: '' }}
-          onSubmit={handleSubmit}
-        >
-          {({
-            values,
-            handleChange,
-            isSubmitting,
-            handleSubmit: formikHandleSubmit,
-          }) => (
-            <Form noValidate onSubmit={formikHandleSubmit}>
-              <FloatingLabel label={t('username')} className="mb-3">
-                <Form.Control
-                  type="text"
-                  placeholder={t('username')}
-                  name="username"
-                  onChange={handleChange}
-                  value={values.username}
-                />
-              </FloatingLabel>
-              <FloatingLabel label={t('password')} className="mb-3">
-                <Form.Control
-                  type="text"
-                  placeholder={t('password')}
-                  name="password"
-                  onChange={handleChange}
-                  value={values.password}
-                />
-                <Form.Control.Feedback tooltip type="invalid">
-                  {t('errors.login')}
-                </Form.Control.Feedback>
-              </FloatingLabel>
-              <Button
-                className="w-100"
-                size="lg"
-                type="submit"
-                disabled={isSubmitDisabled({ values, isSubmitting })}
-              >
-                {t('form.send')}
-              </Button>
-            </Form>
-          )}
-        </Formik>
-      </Col>
-    </Row>
+    <Container>
+      <Row className="py-5">
+        <Col xs="6" className="mx-auto">
+          <h2 className="mb-5">{t('log in')}</h2>
+          <Formik
+            initialValues={{ username: '', password: '' }}
+            onSubmit={handleSubmit}
+          >
+            {({
+              values,
+              handleChange,
+              isSubmitting,
+              handleSubmit: formikHandleSubmit,
+            }) => (
+              <Form noValidate onSubmit={formikHandleSubmit}>
+                <FloatingLabel label={t('username')} className="mb-3">
+                  <Form.Control
+                    type="text"
+                    placeholder={t('username')}
+                    name="username"
+                    onChange={handleChange}
+                    value={values.username}
+                    isInvalid={authFailed}
+                    ref={inputUsernameRef}
+                  />
+                </FloatingLabel>
+                <FloatingLabel label={t('password')} className="mb-3">
+                  <Form.Control
+                    type="text"
+                    placeholder={t('password')}
+                    name="password"
+                    onChange={handleChange}
+                    value={values.password}
+                    isInvalid={authFailed}
+                  />
+                  <Form.Control.Feedback tooltip type="invalid">
+                    {t('errors.login')}
+                  </Form.Control.Feedback>
+                </FloatingLabel>
+                <Button
+                  className="w-100"
+                  size="lg"
+                  type="submit"
+                  disabled={isSubmitDisabled({ values, isSubmitting })}
+                >
+                  {t('form.send')}
+                </Button>
+              </Form>
+            )}
+          </Formik>
+        </Col>
+      </Row>
+    </Container>
   );
 };
 
