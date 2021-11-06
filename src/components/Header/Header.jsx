e5a1f5c78b1442a489caae7761eee9df44f06390
Dmitry Berdnikov <berdnikovdmitry@mail.ru>
2021-11-06 18:50:00 +0300

    add Header

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import {
+  Navbar,
+  Container,
+  Button,
+} from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import { useAuth } from '../../contexts/authContext.jsx';
+
+const Header = () => {
+  const auth = useAuth();
+
+  return (
+    <header>
+      <Navbar bg="white" className="shadow-sm">
+        <Container>
+          <Navbar.Brand as={Link} to="/">ChatApp</Navbar.Brand>
+          {auth.loggedIn && <Button onClick={() => auth.logOut()}>Log out</Button>}
+        </Container>
+      </Navbar>
+    </header>
+  );
+};
+
+export default Header;
