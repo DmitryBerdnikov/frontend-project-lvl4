9a3376247522c3df3f492aba3b41702af7ead468
Dmitry Berdnikov <berdnikovdmitry@mail.ru>
2021-11-11 22:11:52 +0300

    start working on Chat

--- src/components/Chat/Chat.jsx
+++ src/components/Chat/Chat.jsx
@@ -1,5 +1,43 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import axios from 'axios';
+import routes from '../../routes';
 
-const Chat = () => <div>Chat</div>;
+const getAuthHeader = () => {
+  const user = JSON.parse(localStorage.getItem('user'));
+
+  if (user && user.token) {
+    return { Authorization: `Bearer ${user.token}` };
+  }
+
+  return {};
+};
+
+const Chat = () => {
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(routes.dataPath(), { headers: getAuthHeader() });
+        console.log(response);
+      } catch (err) {
+        console.warn('can not fetch data for chat');
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <div className="container py-5 h-100">
+      <div className="row h-100 shadow bg-white rounded">
+        <div className="col-4 col-md-2 bg-light border-end pt-5">
+          Каналы
+        </div>
+        <div className="col">
+          Hello
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Chat;
