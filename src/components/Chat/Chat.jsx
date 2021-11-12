9325e46e92cb9c3bb21666b9da5cb7b832f0339d
Dmitry Berdnikov <berdnikovdmitry@mail.ru>
2021-11-12 22:13:59 +0300

    add ui for Chat: aside, header, messages

--- src/components/Chat/Chat.jsx
+++ src/components/Chat/Chat.jsx
@@ -1,5 +1,13 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
+import {
+  Col,
+  Row,
+  Container,
+  Button,
+  Nav,
+} from 'react-bootstrap';
+import { useTranslation } from 'react-i18next';
 import routes from '../../routes';
 
 const getAuthHeader = () => {
@@ -13,11 +21,18 @@ const getAuthHeader = () => {
 };
 
 const Chat = () => {
+  const { t } = useTranslation();
+  const [data, setData] = useState({
+    channels: [],
+    currentChannelId: null,
+    messages: [],
+  });
+
   useEffect(() => {
     const fetchData = async () => {
       try {
         const response = await axios.get(routes.dataPath(), { headers: getAuthHeader() });
-        console.log(response);
+        setData(response.data);
       } catch (err) {
         console.warn('can not fetch data for chat');
       }
@@ -26,17 +41,57 @@ const Chat = () => {
     fetchData();
   }, []);
 
+  const currentChannel = data.channels.find(({ id }) => id === data.currentChannelId);
+
   return (
-    <div className="container py-5 h-100">
-      <div className="row h-100 shadow bg-white rounded">
-        <div className="col-4 col-md-2 bg-light border-end pt-5">
-          Каналы
-        </div>
-        <div className="col">
-          Hello
-        </div>
-      </div>
-    </div>
+    <Container className="py-5 h-100">
+      <Row className="row h-100 shadow bg-white rounded">
+        <Col className="col-4 col-md-2 border-end pt-5">
+          <div className="d-flex justify-content-between">
+            <span>{t('channels')}</span>
+            <Button size="sm" variant="primary">+</Button>
+          </div>
+          {
+            !!data.channels.length && (
+              <Nav as="ul" variant="pills" className="mt-3 flex-column">
+                {data.channels.map(({ id, name }) => (
+                  <Nav.Item
+                    as="li"
+                    key={id}
+                  >
+                    <Button
+                      variant={data.currentChannelId === id ? 'secondary' : ''}
+                      className="rounded-0 w-100 text-start"
+                    >
+                      {name}
+                    </Button>
+                  </Nav.Item>
+                ))}
+              </Nav>
+            )
+          }
+        </Col>
+        <Col className="p-0 d-flex flex-column">
+          <div className="p-3 border-bottom">
+            { !!currentChannel && (
+              <>
+                <h1 className="h6 mb-0">{`# ${currentChannel.name}`}</h1>
+                <div className="text-muted">{t('messageWithCount.text', { count: data.messages.length })}</div>
+              </>
+            )}
+          </div>
+          <div className="p-5 flex-grow-1">
+            <div className="mb-2">
+              <span className="fw-bold">username: </span>
+              message
+            </div>
+          </div>
+          <div className="px-5 py-3">
+            TODO input
+          </div>
+        </Col>
+      </Row>
+    </Container>
   );
 };
 
