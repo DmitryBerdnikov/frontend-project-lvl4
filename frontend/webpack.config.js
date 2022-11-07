a69b6361c0a4692fc4436d5e30c65abee54abf5a
Dmitry Berdnikov <berdnikovdmitry@mail.ru>
2022-11-07 21:51:46 +0500

    add webpack base packages

---
+++ frontend/webpack.config.js
@@ -0,0 +1,33 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+const mode = process.env.NODE_ENV || 'development';
+
+module.exports = {
+  mode,
+  entry: './src/index.js',
+  plugins: [new MiniCssExtractPlugin()],
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'build'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          {
+            loader: 'postcss-loader',
+            options: {
+              postcssOptions: {
+                plugins: [['postcss-preset-env', {}]],
+              },
+            },
+          },
+        ],
+      },
+    ],
+  },
+};
