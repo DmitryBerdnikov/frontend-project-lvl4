48d0c509143fd6335e643c44cc0ab97f63466d92
DmitryBerdnikov <berdnikovdmitry@mail.ru>
2022-12-11 17:07:22 +0500

    add NotFound page

--- frontend/src/pages/NotFound.jsx
+++ frontend/src/pages/NotFound.jsx
@@ -1,3 +1,15 @@
-const NotFound = () => <div>NotFound</div>;
+import { Link } from 'react-router-dom';
+import Container from 'react-bootstrap/Container';
+
+const NotFound = () => (
+  <Container className="h-100 py-4 d-flex flex-column align-items-center justify-content-center text-center">
+    <h1>Страница не найдена</h1>
+    <p>
+      Но вы можете перейти
+      {' '}
+      <Link to="/">на главную страницу</Link>
+    </p>
+  </Container>
+);
 
 export default NotFound;
