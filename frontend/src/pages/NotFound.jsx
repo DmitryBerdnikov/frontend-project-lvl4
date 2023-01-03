b521334804bb477924e206553d76270df4654156
DmitryBerdnikov <berdnikovdmitry@mail.ru>
2023-01-03 18:52:05 +0500

    fix eslitnt errors

--- frontend/src/pages/NotFound.jsx
+++ frontend/src/pages/NotFound.jsx
@@ -5,9 +5,7 @@ const NotFound = () => (
   <Container className="h-100 py-4 d-flex flex-column align-items-center justify-content-center text-center">
     <h1>Страница не найдена</h1>
     <p>
-      Но вы можете перейти
-      {' '}
-      <Link to="/">на главную страницу</Link>
+      Но вы можете перейти <Link to="/">на главную страницу</Link>
     </p>
   </Container>
 );
